f9f7d006a529300e5fe6dbfb07065184c73603d0
OksanaMashtaler <mashoksana@gmail.com>
2022-11-06 23:12:11 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,47 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const form = document.querySelector('.form');
+const refs = {
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+  btnSubmit: document.querySelector('button[type="submit"]'),
+  form: document.querySelector('.form'),
+};
+refs.form.addEventListener('submit', onFormSubmit);
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// Функція для сабміту форми
+function onFormSubmit(e) {
+  // скидаю налаштування, щоб при сабміті не перезавантажувати сторінку
+  e.preventDefault();
+  // при сабміті записую значення інпутів в перемінні
+  let delay = Number(refs.inputDelay.value);
+  let step = Number(refs.inputStep.value);
+  let amount = Number(refs.inputAmount.value);
+  // щоб викликати ф-цію створення промісів н разів застосовую фор
+  for (let i = 0; i < amount; i += 1) {
+    // виклик ф-ції промісів куди вказую нумерацію і затримку з врахуванням кроку
+    createPromise(i + 1, delay + i * step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+// функція для створення промісів
+function createPromise(position, delay) {
+  // вертаємо в зовнішній код проміс
+  return new Promise((resolve, reject) => {
+    // роблю затримку для виконання промісу
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
